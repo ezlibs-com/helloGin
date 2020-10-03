37427940cae40959a7af3a2f3aec556741e74e86
gminitech <joy.woo@qq.com>
2020-10-03 18:42:21 +0800

    the helloGin2

---
+++ helloGin2/main.go
@@ -0,0 +1,16 @@
+// main.go
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", getHomePage)
+
+	r.Run(":28080")
+}
+
+func getHomePage(c *gin.Context){
+c.String(200, "Hello, Gin")
+}
